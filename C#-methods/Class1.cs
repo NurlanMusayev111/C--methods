f5fbb6d2ea62c56e6f553debb18b195595b83add
nurlanmusayev <nurlan.musayev.2002@inbox.ru>
2023-09-23 01:45:26 +0400

    methods and overloading

---
+++ C#-methods/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal static class Class1
+        {
+            public static void Test()
+            {
+                Console.WriteLine("Test1");
+            }
+
+
+
+            public static void Test(int a)
+            {
+                Console.WriteLine("Test2");
+            }
+        
+            
+            public static void Test(string a)
+            {
+                Console.WriteLine("Test3");
+            }
+        }
+    
+}
--- C#-methods/Program.cs
+++ C#-methods/Program.cs
@@ -1,181 +1,383 @@
-﻿//static void CheckMarried(bool isMarried,int age)
+﻿
+
+
+
+
+
+
+
+
+
+
+
+//Sinifde yazilan kodlar
+
+//1------------------
+
+//static string Test(int num1,string name,int num2 = 100)
 //{
-//    if (isMarried)
-//    {
-//        Console.WriteLine("evlidir -" + age);
-//    }
-//    else
-//    {
-//        Console.WriteLine("Subaydir -" + age);
-//    }
+//    return num1 + name + num2;
 //}
 
-//static void GetAge(int age)
+//Console.WriteLine(Test(5,"Nurlan"));
+
+
+
+
+
+
+
+//2-------------------
+
+//static void Test(int a,params int[] nums)
 //{
-//    bool isMarried = false; 
-//    CheckMarried(isMarried,age);
-//}
-
-
-//int age = 35;
-
-//GetAge(age);
-
-
-
-
-
-//static int GetNumber()
-//{
-//    int number = 100;
-//    return number;  
-//}
-
-
-//int result = GetNumber();
-
-//if(result > 10)
-//{
-//    Console.WriteLine(GetNumber());
-//}
-
-
-
-//static string GetTemp(int temp)
-//{
-//    if(temp > 20)
+//    foreach (int item in nums)
 //    {
-//        return "hot";
-//    }
-//    else
-//    {
-//        return "cold";
+//        Console.WriteLine(item);
 //    }
 //}
 
-
-//string result = GetTemp(30);
-
-//Console.WriteLine(result);
+//Test(1, 2, 3, 4, 5, 6);
 
 
 
 
+//3---------------------
 
 
-//static bool IsSuccess(string employeeName)
-//{
-//    string[] employees = { "Semed", "Tunzale", "Metanet", "Meryem", "Haci", "Aqshin" };
-
-//    foreach(var employee in employees)
-//    {
-//        if(employee == employeeName)
-//        {
-//            return true;
-//        }
-//    } 
-//    return false;
-//}
-
-//Console.WriteLine("Ad daxil edin :");
-//string name = Console.ReadLine();
-
-//bool result = IsSuccess(name);
-
-//if (result)
-//{
-//    Console.WriteLine("Ugurludur");
-//}
-//else
-//{
-//    Console.WriteLine("ugurlu deyil");
-//}
-
-
-
-//static int GetSumOddNumbers(int a,int b)
+//static void Test(int a,params int[] numbers)
 //{
 //    int sum = 0;
-//    for(int i = a; i <= b; i++)
+//    foreach (int item in numbers)
 //    {
-//        if(i%2 == 1)
+//        sum += item;    
+//    }
+
+//    int result = sum * a;
+
+//    Console.WriteLine(result);
+//}
+
+//Test(1, 2, 3, 4, 4, 5);
+
+
+
+//4-----------------------
+
+
+//using MethodOverloading;
+
+
+//Class1.Test(5);
+
+
+//5-------------------
+
+
+//int a = 20;
+
+//int b = a;
+
+//b = 200;
+
+
+//Console.WriteLine(a);
+//Console.WriteLine(b);
+
+//-------------------------------
+
+//string name = "Nurlan";
+
+//string surname = name;
+
+//surname = "Musayev";
+
+//Console.WriteLine(name);
+//Console.WriteLine(surname);
+
+
+//--------------------------------
+
+
+//int[] arr1 = { 2, 3, 4, 5 };
+
+//int[] arr2 = arr1;
+
+//arr2[0] = 100;
+
+//Console.WriteLine(arr1[0]);
+//Console.WriteLine(arr2[0]);
+
+
+
+
+
+
+
+
+
+
+//2) Parametr olaraq string qebul eden metod yazmalisiz. Hemin metod qebul etdiyi stringi tersine formada ekranda gostersin
+
+
+//static string reverStr(string name)
+//{
+//    string result = "";
+//    for (int i = name.Length - 1; i >= 0; i--)
+//    {
+//        result += name[i];
+//    }
+//    return result;
+//}
+
+
+//Console.WriteLine(reverStr("Nurlan"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapın
+
+
+//static string divideNumber(int num)
+//{
+//    if(num % 3 == 0 && num % 7 == 0)
+//    {
+//        return "Eded 3 ve 7 e bolunur";
+//    }
+//    else
+//    {
+//        return "Eded 3 ve 7 e bolunmur";
+//    }
+//}
+
+
+//Console.WriteLine(divideNumber(21));
+
+
+
+
+
+
+//2) n və m ədədləri verilir. Əgər n və m ədədləri hər ikisi cütdürsə n və m ədədlərinin cəmini hesablayın
+
+
+//static int checkEvenNumber(int a, int b)
+//{
+//    int sum = a + b;
+//    if (a % 2 == 0 && b % 2 == 0)
+//    {
+//        return sum;
+//    }
+//    else
+//    {
+//        return 0;
+//    }
+//}
+
+
+//Console.WriteLine(checkEvenNumber(6, 10));
+
+
+
+
+
+//3) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin sayini tapin
+
+
+
+//static int chechOddNumber(int n,int m)
+//{
+//    int count = 0;
+//    for (int i = n; i <= m; i++)
+//    {
+//        if (i % 2 == 1)
+//        {
+//            count++;
+//        }
+//    }return count;  
+//}
+
+
+//Console.WriteLine(chechOddNumber(3,8));
+
+
+
+
+
+//4) Verilmis n ve m (n<m) ededleri arasindaki tek ededlerin cemini tapin
+
+
+//static int findOddNumber(int n,int m)
+//{
+//    int sum = 0;
+//    for(int i = n; i <= m; i++)
+//    {
+//        if (i % 2 == 1)
 //        {
 //            sum += i;
+//        }
+
+//    }
+//    return sum;
+//}
+
+
+//Console.WriteLine(findOddNumber(3,8));
+
+
+
+
+
+//5)Verilmish arrayin icindeki tek ededlerin cemini tapin
+
+
+//static int findArrOddnum(int[] nums)
+//{
+//    int sum = 0;
+//    for (int i = 0; i < nums.Length; i++)
+//    {
+//        if (nums[i] % 2 == 1)
+//        {
+//            sum += nums[i];
 //        }
 //    }
 //    return sum;
 //}
 
 
-//int result = GetSumOddNumbers(5, 10);
+//int[] nums = {2,3,4,5,6,7};
 
-//Console.WriteLine(result);
-
-
+//Console.WriteLine(findArrOddnum(nums));
 
 
 
 
-//static bool login(string username, string password)
+
+//6)Verilmish arrayin icindeki cut ededlerin sayini tapin
+
+
+//static int findEvenNum(int[] nums)
 //{
-//    if(username == "nurlan111" && password =="nurlan123")
+//    int count = 0;
+//    for(int i = 0; i < nums.Length; i++)
 //    {
-//        return true;
-//    }
-
-//    return false;
-//}
-
-
-
-//Console.WriteLine("username daxil edin :");
-
-//string username = Console.ReadLine();
-
-
-//Console.WriteLine("password daxil edin :");
-
-//string password = Console.ReadLine();
-
-
-//if (login(username, password))
-//{
-//    Console.WriteLine("Login success");
-//}
-//else
-//{
-//    Console.WriteLine("username or passowrd is incorrect");
-//}
-
-
-
-//static bool Search(string studentName)
-//{
-//    string[] students = { "Rufet", "Semed", "Oruc", "Aqsin", "Haci" };
-//    foreach(var student in students)
-//    {
-//        if(student == studentName)
+//        if (nums[i] % 2 == 0)
 //        {
-//            return true;
+//            count++;
 //        }
 //    }
-//    return false;
+//    return count;
 //}
 
 
-//Console.WriteLine("Adi daxil edin :");
-
-//string name = Console.ReadLine();
-
-//bool result = Search(name);
+//int[] nums = { 2, 12, 4, 5, 6, 7, 8 };
 
 
-//if (result)
+//Console.WriteLine(findEvenNum(nums));
+
+
+
+
+//7) Verilmis n tam ededinin sade ve ya murekkeb oldugunu tapin
+
+//static string checkNums(int a)
 //{
-//    Console.WriteLine("Student is found");
+//    int count = 0;
+
+//    for(int i = 1; i <= a; i++)
+//    {
+//        if(a%i == 0)
+//        {
+//            count++;
+//        }
+//    }if(count > 2)
+//    {
+//        return "Eded murekkebdir";
+//    }
+//    else
+//    {
+//        return "Eded sadedir";
+//    }
+
 //}
-//else
+
+
+//Console.WriteLine(checkNums(7));
+
+
+
+
+
+
+//8) n ededi gelir. 2-ni hansisa quvvete yukseltdikde n-in alinan cavaba beraber olub-olmamasını tapın
+
+
+
+
+
+
+
+
+//9)Verilmish arrayin-in elementlerinin icinden 1-20 arasinda olanlarinin hasilini tapmaq
+
+
+//static int multNum(int[] nums)
 //{
-//    Console.WriteLine("Not found");
+//    int multipl = 1;
+//    for(int i = 0; i < nums.Length; i++)
+//    {
+//        if (nums[i]>1 && nums[i] <20)
+//        {
+//            multipl *= nums[i]; 
+//        }
+//    }
+//    return multipl; 
 //}
+
+
+//int[] nums = {2,3,-1,4,5,23,24,25};
+
+//Console.WriteLine(multNum(nums));
+
+
+
+
+
+//10)Verilmish arrayin elementlerininden cut olanlarinin ceminin kvadratini tapmaq
+
+
+//static int powEvenNum(int[] nums)
+//{
+//    int sum = 0;
+
+//    for (int i = 0; i < nums.Length; i++)
+//    {
+
+//        if (nums[i] % 2 == 0)
+//        {
+//            sum += nums[i];
+//        }
+//    }
+
+//    return sum * sum;
+//}
+
+
+
+//int[] nums = { 2, 3, 4, 5 };
+
+
+//Console.WriteLine(powEvenNum(nums));
 
